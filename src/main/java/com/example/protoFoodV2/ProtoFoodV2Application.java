a5c6e772e6e24d241e7383bf5fb4e45f40263ef2
vijit-saxena <contact.vijit.saxrna@gmail.com>
2023-05-11 12:44:17 +0530

    Initial Setup

---
+++ src/main/java/com/example/protoFoodV2/ProtoFoodV2Application.java
@@ -0,0 +1,13 @@
+package com.example.protoFoodV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoFoodV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoFoodV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/protoFoodV2/ProtoFoodV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protoFoodV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoFoodV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
